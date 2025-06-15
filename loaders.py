07580fc7244078afa14be1ebdfeac8f04939d142
itsoutchy-projects <itsoutchy55@yahoo.com>
2025-06-15 12:30:23 +0100

    forgot to commit the audio loader oops

--- loaders.py
+++ loaders.py
@@ -8,3 +8,8 @@ class Image:
         If `scene` is the menu, just set it to "" (still put in the variable tho)
         """
         return pygame.image.load(os.path.join("images", scene, filename))
+    
+class Sound:
+    def Load(filename):
+        with open(os.path.join("sounds", filename)) as f:
+            return pygame.mixer.Sound(f)
